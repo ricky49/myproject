4401e5e0a62ca46a90b9fa9e76a37730133216fa
Ricky Soto (ClaroDom) <Ricky Soto@corp.codetel.com.do>
2017-12-14 16:09:50 -0400

    alert added to login component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -5,6 +5,8 @@ import { UsersService } from 'app/services/users/users.service';
 import { Users } from 'app/models/users';
 import { forEach } from '@angular/router/src/utils/collection';
 import { Router } from '@angular/router';
+import {Subject} from 'rxjs/Subject';
+import {debounceTime} from 'rxjs/operator/debounceTime';
 
 
 
@@ -13,34 +15,43 @@ import { Router } from '@angular/router';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
-users: Users [];
+  users: Users[];
+  staticAlertClosed = false;
+  failMessage: string;
+  private _fail = new Subject<string>();
+
   constructor(private usersService: UsersService,
-              private router: Router) { }
+    private router: Router) { }
 
   ngOnInit() {
     this.getUsers();
+    this._fail.subscribe((message) => this.failMessage = message);
+    debounceTime.call(this._fail, 3000).subscribe(() => this.failMessage = null);
   }
 
-loginUser(f: NgModel) {
+  loginUser(f: NgModel) {
 
-  const currentUser = f.value;
+    const currentUser = f.value;
 
-  this.users.every((x, index) => {
+    this.users.every((x, index) => {
 
-    if ( x.mail === currentUser.mail && x.password === currentUser.password ) {
-      this.usersService.setUserLogIn();
-      this.router.navigate(['dashboard']);
-      return false;
-    } else {
-      console.log('autenticacion fallo');
-    }
+      if (x.mail === currentUser.mail && x.password === currentUser.password) {
+        this.usersService.setUserLogIn();
+        this.router.navigate(['dashboard']);
+        return false;
 
-  });
+      } else {
+        this._fail.next('Usuario o contraseña incorrecto');
+      }
 
-}
- getUsers() {
- this.usersService.getUsers()
- .subscribe(users => this.users = users);
- }
+    });
+
+  }
+  getUsers() {
+    this.usersService.getUsers()
+      .subscribe(users => this.users = users);
+  }
+
 }
